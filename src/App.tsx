87c17aa8da3630bfc73a78d461c4b471ae3f41f0
FlowerF <1647231392@qq.com>
2022-05-11 17:59:34 +0800

    feat: 功能完善

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,41 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Header from './Header';
 
 function App() {
   const [source, setSource] = useState('');
   const [loading, setLoading] = useState(false);
-  const [type, setType] = useState(true); // true 表示动图
 
   const getSource = (type: boolean) => {
     setLoading(true);
-    if (type) {
-      axios
-        .get(
-          'https://service-aja60qcs-1305624698.gz.apigw.tencentcs.com/release/api/dynamic'
-        )
-        .then((res) => {
-          setSource(res.data.data.url);
-        })
-        .catch(() => {})
-        .finally(() => {
-          setLoading(false);
-        });
-    } else {
-      axios
-        .get(
-          'https://service-aja60qcs-1305624698.gz.apigw.tencentcs.com/release/api/static'
-        )
-        .then((res) => {
-          setSource(res.data.data.url);
-        })
-        .catch(() => {})
-        .finally(() => {
-          setLoading(false);
-        });
-    }
+    axios
+      .get(
+        `https://service-aja60qcs-1305624698.gz.apigw.tencentcs.com/release/api/${
+          type ? 'dynamic' : 'static'
+        }`
+      )
+      .then((res) => {
+        setSource(res.data.data.url);
+      })
+      .catch(() => {})
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   useEffect(() => {
-    getSource(type);
+    getSource(true);
   }, []);
 
   return (
-    <div className='min-h-screen main-bg relative flex items-center'>
+    <div className='min-h-screen main-bg relative flex justify-center flex-col'>
       <div className='text-center w-full pb-16'>
+        <Header />
         {loading ? (
-          <div className='font-semibold text-xl text-black flex items-center justify-center'>
+          <div
+            className='font-semibold text-xl text-black flex 
+          items-center justify-center h-[300px]'
+          >
             <svg
               className='animate-spin h-5 w-5 mr-3 text-black'
               xmlns='http://www.w3.org/2000/svg'
@@ -67,23 +59,19 @@ function App() {
             哆啦哆啦哆啦哆啦
           </div>
         ) : (
-          <img src={source} alt='doraemon' className='w-3/4 inline-block' />
+          <div className='h-[300px]'>
+            <img src={source} alt='doraemon' className='w-3/4 inline-block' />
+          </div>
         )}
       </div>
 
       <div className='flex absolute bottom-20 w-full justify-center'>
-        <button
-          className='button'
-          onClick={() => {
-            setType(!type);
-            getSource(!type);
-          }}
-        >
-          我要{type ? '静' : '动'}图
+        <button className='button' onClick={() => getSource(false)}>
+          不动的
         </button>
 
-        <button className='button ml-4' onClick={() => getSource(type)}>
-          获取表情
+        <button className='button ml-4' onClick={() => getSource(true)}>
+          会动的
         </button>
       </div>
     </div>
---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className='text-center mb-20'>
+      <h1 className='text-3xl font-bold text-shadow'>luv and peace ya</h1>
+      <h3 className='text-right w-3/4 text-base mt-1'>—— AKASowhat</h3>
+    </div>
+  );
+};
+
+export default Header;
